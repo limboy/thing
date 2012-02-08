97ee17c6f60f97bd17a393ad158ae02c6a9772ef
lzyy <healdream@gmail.com>
2012-02-09 01:54:51 +0800

    adjust dir

--- setup.py
+++ setup.py
@@ -2,23 +2,25 @@ from setuptools import setup
 import os
 
 setup(
-    name='Thing',
-    version='0.1',
-    url='http://github.com/lzyy/thing/',
-    license='BSD',
-    author='lzyy',
-    author_email='healdream@gmail.com',
-    description='a tiny active record orm based on sqlalchemy',
-    long_description=open('./README.md').read(),
-    zip_safe=False,
-    platforms='any',
-    install_requires=[
+    name = 'Thing',
+    version = '0.1',
+    url = 'http://github.com/lzyy/thing/',
+    license = 'BSD',
+    author = 'lzyy',
+    author_email = 'healdream@gmail.com',
+    description = 'a tiny active record orm based on sqlalchemy',
+    long_description = open('./README.md').read(),
+    zip_safe = False,
+    platforms = 'any',
+    packages = ["src"],
+    include_package_data = True,
+    install_requires = [
         'sqlalchemy',
         'blinker',
         'formencode',
         'mysql-python',
     ],
-    classifiers=[
+    classifiers = [
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
